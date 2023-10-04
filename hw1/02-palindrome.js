0748e8c148ddb1bed9a956084f488bd564e62c44
mattsloe <mattsloe@gmail.com>
2023-10-04 16:48:59 -0700

    work on #2, javascript done

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,27 @@
 const elem = document.querySelector('input');
+const result = document.querySelector('div > div');
 
 elem.addEventListener('input', handleInput);
+
+function handleInput(){
+    const inValue = elem.value;
+    const isNegative = inValue < 0;
+    const isPalindrome = isNumPalindrome(inValue);
+
+    if(isNegative){
+        result.textContent = 'Negative';
+    } else if(isPalindrome){
+        result.textContent = 'Palindrome';
+    } else {
+        result.textContent = 'Not a palindrome';
+    }
+}
+
+function isNumPalindrome(num){
+    const numStr = num.toString();
+    const reversedNumStr = numStr.split('').reverse().join('');
+        //split the string into an array
+        //reverse the array
+        //join array into string
+    return numStr === reversedNumStr; //are they the same?
+}
