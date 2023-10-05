4b27d8faab026a89a26cfa7496dd2de067aa64fd
mattsloe <mattsloe@gmail.com>
2023-10-04 22:14:29 -0700

    work on #2, linting

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -3,25 +3,28 @@ const result = document.querySelector('div > div');
 
 elem.addEventListener('input', handleInput);
 
-function handleInput(){
+function handleInput() {
     const inValue = elem.value;
     const isNegative = inValue < 0;
     const isPalindrome = isNumPalindrome(inValue);
 
-    if(isNegative){
+    if (isNegative) {
         result.textContent = 'Negative';
-    } else if(isPalindrome){
+        result.style.color = 'red';
+    } else if (isPalindrome) {
         result.textContent = 'Palindrome';
+        result.style.color = 'green';
     } else {
         result.textContent = 'Not a palindrome';
+        result.style.color = 'red';
     }
 }
 
-function isNumPalindrome(num){
+function isNumPalindrome(num) {
     const numStr = num.toString();
     const reversedNumStr = numStr.split('').reverse().join('');
-        //split the string into an array
-        //reverse the array
-        //join array into string
+    //split the string into an array
+    //reverse the array
+    //join array into string
     return numStr === reversedNumStr; //are they the same?
 }
