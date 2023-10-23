45953c0768a2f6ca90a83891d760ff1878572bc4
mattsloe <mattsloe@gmail.com>
2023-10-23 14:14:32 -0700

    adjusted for code review

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -4,27 +4,27 @@ const result = document.querySelector('div > div');
 elem.addEventListener('input', handleInput);
 
 function handleInput() {
-    const inValue = elem.value;
-    const isNegative = inValue < 0;
-    const isPalindrome = isNumPalindrome(inValue);
+  const inValue = elem.value;
+  const isNegative = inValue < 0;
+  const isPalindrome = isNumPalindrome(inValue);
 
-    if (isNegative) {
-        result.textContent = 'Negative';
-        result.style.color = 'red';
-    } else if (isPalindrome) {
-        result.textContent = 'Palindrome';
-        result.style.color = 'green';
-    } else {
-        result.textContent = 'Not a palindrome';
-        result.style.color = 'red';
-    }
+  if (isNegative) {
+    result.textContent = 'Negative';
+    result.style.color = 'red';
+  } else if (isPalindrome) {
+    result.textContent = 'Palindrome';
+    result.style.color = 'green';
+  } else {
+    result.textContent = 'Not a palindrome';
+    result.style.color = 'red';
+  }
 }
 
 function isNumPalindrome(num) {
-    const numStr = num.toString();
-    const reversedNumStr = numStr.split('').reverse().join('');
-    //split the string into an array
-    //reverse the array
-    //join array into string
-    return numStr === reversedNumStr; //are they the same?
+  const numStr = num.toString();
+  const reversedNumStr = numStr.split('').reverse().join('');
+  // split the string into an array
+  // reverse the array
+  // join array into string
+  return numStr === reversedNumStr; // are they the same?
 }
